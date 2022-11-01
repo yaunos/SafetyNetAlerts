3ac1b33bf5594584bb52052bd07438c5652136b6
DevPC <a.roche.c@gmail.com>
2022-11-01 02:51:35 +0100

    Lancement de l'appli SafetyNetAlertsApplication avec les services package et model avec BusinessService déclaré en tant que Bean dans le contexte Spring

--- src/main/java/com/SafetyNetAlerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -1,5 +1,8 @@
 package com.SafetyNetAlerts;
 
+import com.SafetyNetAlerts.model.SafetyNetAlerts;
+import com.SafetyNetAlerts.service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SafetyNetAlertsApplication implements CommandLineRunner{
 
+	@Autowired
+	private BusinessService bs;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("SafetyNet Alerts Application is up and running !");
+		SafetyNetAlerts sna = bs.getSafetyNetAlerts();
+		System.out.println(sna);
 	}
 
 
